f3a9b6a038f90a4a60a68ce1c2c31fe469f8d4a4
indigenizemilwaukeeproject <83777895+indigenizemilwaukeeproject@users.noreply.github.com>
2021-08-20 09:03:45 -0500

    updated google link

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1PjATYdWSb9-loXbIu09RaQVfVjPFgWP_lSKPhW25_kI/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1pR-d2VLOHZIzATFjTA5-U__Uv8zlKjqi26DUmu2h_Rs/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
